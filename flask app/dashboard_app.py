976267be02b6bea2be4e745e9767111dada7cf90
knowledge-corner <vaidehi.nair@knowledgecorner.in>
2024-09-10 12:24:45 +0530

    update

--- flask app/dashboard_app.py
+++ flask app/dashboard_app.py
@@ -6,7 +6,18 @@ app = Flask(__name__)
 
 @app.route("/")   # url pattern
 def home_page():
-    return render_template("homepage.html", cities = cities)
+    total_sales = df.Sales.sum()
+    total_profits = df.Profit.sum()
+    return render_template("homepage.html", cities = cities, sales = total_sales,
+                           profit = total_profits, selected_city = "Select City")
+
+@app.route("/filter/<city>")   # url pattern
+def filter_by_city(city):
+    filter_df = df[df.City == city]
+    total_sales = filter_df.Sales.sum()
+    total_profits = filter_df.Profit.sum()
+    return render_template("homepage.html", cities = cities, sales = total_sales,
+                           profit = total_profits, selected_city = city)
 
 
 if __name__ == "__main__" :

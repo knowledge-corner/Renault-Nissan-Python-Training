5df4e1bbe479fdf2121587f96881ddd7986506a8
knowledge-corner <vaidehi.nair@knowledgecorner.in>
2024-09-10 14:08:59 +0530

    charts

--- flask app/dashboard_app.py
+++ flask app/dashboard_app.py
@@ -1,24 +1,28 @@
 # ---------- All imports
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
 from data import *
 
 app = Flask(__name__)
+
+
 
 @app.route("/")   # url pattern
 def home_page():
     total_sales = df.Sales.sum()
     total_profits = df.Profit.sum()
+    chart = (fig_bar_sales(df), fig_bar_profit(df), fig_line_profit(df), fig_line_sales(df))
     return render_template("homepage.html", cities = cities, sales = total_sales,
-                           profit = total_profits, selected_city = "Select City")
+                           profit = total_profits, selected_city = "Select City", chart = chart)
 
 @app.route("/filter/<city>")   # url pattern
 def filter_by_city(city):
     filter_df = df[df.City == city]
     total_sales = filter_df.Sales.sum()
     total_profits = filter_df.Profit.sum()
+    chart = (fig_bar_sales(filter_df), fig_bar_profit(filter_df), 
+             fig_line_profit(filter_df), fig_line_sales(filter_df))
     return render_template("homepage.html", cities = cities, sales = total_sales,
-                           profit = total_profits, selected_city = city)
-
+                           profit = total_profits, selected_city = city, chart = chart)
 
 if __name__ == "__main__" :
     app.run()
--- flask app/data.py
+++ flask app/data.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from io import BytesIO
+import base64
 
 # From SQLITE3 -----------
 # from sqlalchemy import create_engine
@@ -19,4 +21,83 @@ cities = list(df["City"].unique())
 years = list(df["Year"].unique())
 months = list(df["Month"].unique())
 products = list(df["Product"].unique())
+
+def fig_bar_sales(data) :
+    # Sales by product bar
+    fig = plt.figure(figsize=(10, 5))
+    sns.barplot(x='Product', y='Sales', hue='Product', data=df, estimator="sum",
+                    errorbar=None)
+    plt.title("Sales by Product", loc = "left", fontdict={"fontsize" : 16}, pad = 10)
+    plt.xticks(rotation = 20, size = 8)
+    
+    # Save the plot to a BytesIO object
+    img = BytesIO()
+    fig.savefig(img, format='png')
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode()
+
+    # Close the figure to release memory
+    plt.close(fig)
+
+    return plot_url
+
+
+def fig_bar_profit(data) :
+    # profit by product bar
+    fig = plt.figure(figsize=(10, 5))
+    sns.barplot(x='Product', y='Profit', hue='Product', data=df, estimator="sum",
+                    errorbar=None)
+    plt.title("Profit by Product", loc = "left", fontdict={"fontsize" : 16}, pad = 10)
+    plt.xticks(rotation = 20, size = 8)
+    
+    # Save the plot to a BytesIO object
+    img = BytesIO()
+    fig.savefig(img, format='png')
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode()
+
+    # Close the figure to release memory
+    plt.close(fig)
+
+    return plot_url
+
+def fig_line_sales(data) :
+    # sales over months line
+    fig = plt.figure(figsize=(10, 5))
+    sns.lineplot(x='Date', y='Sales', data=df, estimator="sum", color="teal",
+                    errorbar=None)
+    plt.title("Sales over Months", loc = "left", fontdict={"fontsize" : 16}, pad = 10)
+    plt.xticks(rotation = 20, size = 8)
+    
+    # Save the plot to a BytesIO object
+    img = BytesIO()
+    fig.savefig(img, format='png')
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode()
+
+    # Close the figure to release memory
+    plt.close(fig)
+
+    return plot_url
+
+
+def fig_line_profit(data) :
+    # profit over months line
+    fig = plt.figure(figsize=(10, 5))
+    sns.lineplot(x='Date', y='Profit', data=df, estimator="sum", color="teal",
+                    errorbar=None)
+    plt.title("Profit over Months", loc = "left", fontdict={"fontsize" : 16}, pad = 10)
+    plt.xticks(rotation = 20, size = 8)
+
+    
+    # Save the plot to a BytesIO object
+    img = BytesIO()
+    fig.savefig(img, format='png')
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode()
+
+    # Close the figure to release memory
+    plt.close(fig)
+
+    return plot_url
 
